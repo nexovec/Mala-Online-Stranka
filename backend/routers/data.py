06088576259be625aa84ee54c4b3d79ac035806b
yoptohlejepeta <peta.kotlan@seznam.cz>
2024-01-20 04:09:06 +0100

    pocet obyvatel, spider aaaaaa

--- backend/routers/data.py
+++ backend/routers/data.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, HTTPException
 import pandas as pd
 import json
+import plotly.express as px
+import polars as pl
 
 router = APIRouter(prefix="/data", tags=["Data"])
 
 data_df = pd.read_parquet("data/data.parquet").dropna(axis=1, how="all")
+pocet_obyvatel = pd.read_csv("data/pocet_obyvatel.csv")
 
 @router.get("/")
 def read_data(
@@ -51,7 +54,7 @@ def get_rozsahy(metricId: int):
     return res
 
 
-import polars as pl
+
 ukazatele = pl.read_csv("data/cis_ukazatelu.csv")
 uzemi = pl.read_csv("data/cis_uzemi.csv")
 def get_yearly_matrices(year):
@@ -71,4 +74,72 @@ def get_plotly_graph(uzemiid: str):
     ukazatel = 10300
     ukazatel_nazev = ukazatele.filter(pl.col("kodukaz") == ukazatel).select("nazev").to_pandas()["nazev"][0]
     fig = px.line(matrix, x="rok", y=str(ukazatel), title=f"{uzemi_nazev}: {ukazatel_nazev}")
+@router.get("/spider")
+def get_spider(
+    city: int,
+    year: int,
+):
+    df = data_df[["rok", "kodukaz", "koduzemi", "hodnota"]]
+    df = df.loc[df["rok"] == year]
+
+    vol_cas = [141434, 141432, 600808, 600806]
+    vol_casdf = df.loc[df["kodukaz"].isin(vol_cas)]
+    vol_score = vol_casdf.loc[vol_casdf["koduzemi"] == city, "hodnota"].sum()
+    vol_score = vol_score / vol_casdf["hodnota"].max()
+    vol_score = vol_score / pocet_obyvatel.loc[pocet_obyvatel["koduzemi"] == city, "hodnota"].values[0]
+    # 141434 - kulturni a osvetova plocha
+    # 141432 - sportoviste a rekrea. plocha
+    # 600808 - hriste
+    # 600806 - koupaliste
+
+    priroda = [141000, 141431]
+    priroda_df = df.loc[df["kodukaz"].isin(priroda)]
+    prir_score = priroda_df.loc[priroda_df["koduzemi"] == city, "hodnota"].sum()
+    prir_score = prir_score / priroda_df["hodnota"].max()
+    prir_score = prir_score / pocet_obyvatel.loc[pocet_obyvatel["koduzemi"] == city, "hodnota"].values[0]
+    # lesní půda
+    # zeleň
+
+    vzdelani = [40300, 40590]
+    vzdelani_df = df.loc[df["kodukaz"].isin(vzdelani)]
+    vzdelani_score = vzdelani_df.loc[vzdelani_df["koduzemi"] == city, "hodnota"].sum()
+    vzdelani_score = vzdelani_score / vzdelani_df["hodnota"].max()
+    vzdelani_score = vzdelani_score / pocet_obyvatel.loc[pocet_obyvatel["koduzemi"] == city, "hodnota"].values[0]
+    # materske skoly
+    # zakladni skoly
+
+    bydleni = [402120]
+    bydleni_df = df.loc[df["kodukaz"].isin(bydleni)]
+    bydleni_score = bydleni_df.loc[bydleni_df["koduzemi"] == city, "hodnota"].sum()
+    bydleni_score = bydleni_score / bydleni_df["hodnota"].max()
+    bydleni_score = bydleni_score / pocet_obyvatel.loc[pocet_obyvatel["koduzemi"] == city, "hodnota"].values[0]
+    # byty
+
+    zdravotnictvi = [70300, 71000, 70200]
+    zdravotnictvi_df = df.loc[df["kodukaz"].isin(zdravotnictvi)]
+    zdravotnictvi_score = zdravotnictvi_df.loc[zdravotnictvi_df["koduzemi"] == city, "hodnota"].sum()
+    zdravotnictvi_score = zdravotnictvi_score / zdravotnictvi_df["hodnota"].max()
+    zdravotnictvi_score = zdravotnictvi_score / pocet_obyvatel.loc[pocet_obyvatel["koduzemi"] == city, "hodnota"].values[0]
+    # nemocnice
+    # lékarny
+    # zdravotni strediska
+
+    df = pd.DataFrame({
+        "krit": ["Zdravotnictví", "Bydlení", "Příroda", "Vzdělání", "Volný čas"],
+        "values": [
+            zdravotnictvi_score,
+            bydleni_score,
+            prir_score,
+            vzdelani_score,
+            vol_score
+        ]
+    }
+    )
+
+    df["values"] = (df["values"] - df["values"].min()) / (df["values"].max() - df["values"].min())
+
+    df.fillna(0, inplace=True)
+
+    fig = px.line_polar(df, r="values", theta="krit", line_close=True, range_r=[0, 1])
+    
     return fig.to_json()
