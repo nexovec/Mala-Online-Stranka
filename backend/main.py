4504a5dcde68f7794efe2ca64e9f9e29b5f6ded2
nexovec <hraccesky@gmail.com>
2024-01-20 09:00:22 +0100

    added CORS

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from routers import data, ciselnik
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="HackujStat V5",
     docs_url="/docs",
     redoc_url=None,
+)
+
+
+origins = [
+    "*",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
