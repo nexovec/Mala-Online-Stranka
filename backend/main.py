df9dc5771215083b78ecfda95a6df2c5573964ce
nexovec <hraccesky@gmail.com>
2024-01-23 12:31:55 +0100

    feat: asserts that BACKEND_URL is set in .env

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,26 @@
 from fastapi import FastAPI
 from routers import data, ciselnik
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+import dotenv
+import os
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    dotenv.load_dotenv()
+    url = os.environ.get("BACKEND_URL")
+    if not url or url == "":
+        raise Exception("BACKEND_URL is not set in the .env file")
+    print("Moje URL je " + url, flush=True)
+    yield
 
 app = FastAPI(
     title="HackujStat V5",
     docs_url="/docs",
     redoc_url=None,
+    lifespan=lifespan,
 )
+
 
 
 origins = [
